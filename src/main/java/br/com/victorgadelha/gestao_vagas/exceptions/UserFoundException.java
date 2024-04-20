144aacf6fead19c9f8aaa5d2abd892f0421e2d7e
João Victor Gadelha <joaovictor_gadelha@live.com>
2024-04-20 20:10:25 -0300

    feat: Tratando erro de criação de usuário com try/catch

--- src/main/java/br/com/victorgadelha/gestao_vagas/exceptions/UserFoundException.java
+++ src/main/java/br/com/victorgadelha/gestao_vagas/exceptions/UserFoundException.java
@@ -2,6 +2,6 @@ package br.com.victorgadelha.gestao_vagas.exceptions;
 
 public class UserFoundException extends RuntimeException {
     public UserFoundException() {
-        super("Uuário já existe");
+        super("Usuário já existe");
     }
 }
--- src/main/java/br/com/victorgadelha/gestao_vagas/modules/candidate/UseCases/CreateCandidateUseCase.java
+++ src/main/java/br/com/victorgadelha/gestao_vagas/modules/candidate/UseCases/CreateCandidateUseCase.java
@@ -1,7 +1,26 @@
 package br.com.victorgadelha.gestao_vagas.modules.candidate.UseCases;
 
-import br.com.victorgadelha.gestao_vagas.modules.candidate.CandidateEntity;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+import br.com.victorgadelha.gestao_vagas.exceptions.UserFoundException;
+import br.com.victorgadelha.gestao_vagas.modules.candidate.CandidateEntity;
+import br.com.victorgadelha.gestao_vagas.modules.candidate.CandidateRepository;
+
+@Service
 public class CreateCandidateUseCase {
-    execute(CandidateEntity)
+
+    @Autowired
+    private CandidateRepository candidateRepository;
+
+    public CandidateEntity execute(CandidateEntity candidateEntity) {
+        this.candidateRepository
+                .findByUsernameOrEmail(
+                        candidateEntity.getUsername(),
+                        candidateEntity.getEmail())
+                .ifPresent((user) -> {
+                    throw new UserFoundException();
+                });
+        return this.candidateRepository.save(candidateEntity);
+    };
 }
