6d570eedc8402c6e4a616c2a521b4c203b42761f
João Victor Gadelha <joaovictor_gadelha@live.com>
2024-04-21 00:24:08 -0300

    feat: Criando controlador para job

--- src/main/java/br/com/victorgadelha/gestao_vagas/modules/company/entities/JobEntity.java
+++ src/main/java/br/com/victorgadelha/gestao_vagas/modules/company/entities/JobEntity.java
@@ -5,12 +5,14 @@ import java.util.UUID;
 
 import org.hibernate.annotations.CreationTimestamp;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 
 @Entity(name = "job")
@@ -22,11 +24,16 @@ public class JobEntity {
     private UUID id;
     private String description;
     private String benefits;
+
+    @NotBlank(message = "Esse campo é obrigatório")
     private String level;
 
     @ManyToOne()
-    @JoinColumn(name = "company_id", nullable = false)
+    @JoinColumn(name = "company_id", insertable = false, updatable = false)
     private CompanyEntity companyEntity;
+
+    @Column(name = "company_id", nullable = false)
+    private UUID companyId;
 
     @CreationTimestamp
     private LocalDateTime createdAt;
