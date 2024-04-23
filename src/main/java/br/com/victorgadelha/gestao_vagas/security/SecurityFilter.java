d73c0e085655987baa8087522f375987c1230a63
João Victor Gadelha <joaovictor_gadelha@live.com>
2024-04-23 11:58:31 -0300

    feat: refatorando job controller

--- src/main/java/br/com/victorgadelha/gestao_vagas/security/SecurityFilter.java
+++ src/main/java/br/com/victorgadelha/gestao_vagas/security/SecurityFilter.java
@@ -27,7 +27,6 @@ public class SecurityFilter extends OncePerRequestFilter {
 
         SecurityContextHolder.getContext().setAuthentication(null);
         String header = request.getHeader("Authorization");
-        System.out.println(header);
 
         if (header != null) {
             var subjectToken = this.jwtProvider.validateToken(header);
